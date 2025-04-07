7b30e82764dcfa81bc0533e823e400a435cdfd0d
Stochastic Biology Group <50171196+StochasticBiology@users.noreply.github.com>
2025-04-07 09:48:17 +0200

    Add files via upload

--- analyse.R
+++ analyse.R
@@ -1,14 +1,18 @@
 library(ggplot2)
 library(ggpubr)
 
-df = read.csv("output.csv")
+expt = "sample"
 
+if(expt == "sample") {
+  df = read.csv("output-sample.csv")
+} else {
+  df = read.csv("output-turnover.csv")
+}
 df$Vph = (df$SDh)**2/(df$Eh*(1-df$Eh))
 
 ggplot(df, aes(x=t, y=Eh, color=factor(ds))) + geom_line() + facet_grid(h0 ~ nstar)
 ggplot(df, aes(x=t, y=SDh, color=factor(ds))) + geom_line() + facet_grid(h0 ~ nstar)
 ggplot(df, aes(x=t, y=Vph, color=factor(ds))) + geom_line() + facet_grid(h0 ~ nstar)
-ggplot(df, aes(x=t, y=En, color=factor(ds))) + geom_line() + facet_grid(h0 ~ nstar)
 ggplot(df, aes(x=t, y=Vph*nstar, color=factor(ds))) + geom_line() + facet_grid(h0 ~ nstar)
 
 df[df$h0 == 0.5 & df$ds == 0.25 & df$nstar == 160 & df$t == 999,]
@@ -16,7 +20,7 @@ df[df$h0 == 0.5 & df$ds == 0.25 & df$nstar == 160 & df$t == 999,]
 # Vph ~ t / N
 ggarrange(
   ggplot(df, aes(x=t, y=Vph, color=factor(ds), group=paste(nstar,h0))) + 
-  geom_line() + facet_grid(~ ds) + xlim(0,10),
+    geom_line() + facet_grid(~ ds) + xlim(0,10),
   ggplot(df, aes(x=t, y=Vph*nstar, color=factor(ds), group=paste(nstar,h0))) + 
     geom_line() + facet_grid(~ ds) + xlim(0,10) ,
   nrow=2
@@ -42,11 +46,11 @@ g.vts
 ggplot(df, aes(x=Eh, y=Vph)) + 
   #geom_point(shape=21, color="#FFFFFF00", aes(fill = factor(nstar))) +
   geom_line(linewidth=0.5, aes(color=factor(ds), group=paste(nstar,h0))) 
-  
+
 
 ggplot() +
   geom_line(data=df, aes(x=Eh, y=Vph, color=factor(ds), group=paste(nstar, ds, h0))) +
- geom_point(data=df[df$t == max(df$t),], aes(x=Eh, y=Vph, shape=factor(nstar), group=paste(ds,h0))) +
+  geom_point(data=df[df$t == max(df$t),], aes(x=Eh, y=Vph, shape=factor(nstar), group=paste(ds,h0))) +
   theme_minimal()
 
 g.sd = ggplot() +
@@ -69,7 +73,7 @@ print(g.vp1)
 g.vp1 + scale_y_log10()
 
 sf = 2
-png("set-illus.png", width=600*sf, height=600*sf, res=72*sf)
+png(paste0("set-illus-", expt", .png", collapse=""), width=600*sf, height=600*sf, res=72*sf)
 ggarrange(g.sd,
           ggarrange(g.vp, g.vp1, nrow=1), nrow=2)
 dev.off()
